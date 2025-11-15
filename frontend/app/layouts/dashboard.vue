47aef265d3549cc0b54d8f0cb7c91422a80594ac
Atia-Farha <119663174+Atia-Farha@users.noreply.github.com>
2025-11-15 21:21:57 +0600

    chore: Add layout and index file

---
+++ frontend/app/layouts/dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <main>
+            <slot />
+        </main>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+}
+
+main {
+    flex-grow: 1;
+    width: 100%;
+    padding: 2em 1em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3em;
+}
+</style>
---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+definePageMeta({
+    layout: 'dashboard'
+})
+
+useHead({
+    title: 'Resume Buildr',
+    htmlAttrs: { lang: 'en' },
+    meta: [{
+        name: 'Resume Buildr',
+        content: 'AI assisted resume maker for quick resumes'
+    }]
+})
+</script>
+
+<template>
+    <div>Hello</div>
+</template>
