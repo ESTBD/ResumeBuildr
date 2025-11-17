b141100d431124435275975e684a89577b761408
SM Sadat <smsadat788@gmail.com>
2025-11-17 23:31:15 +0600

    fuck: api rewrite

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CVformViewSet
 
 router = DefaultRouter()
-router.register(r'form', CVformViewSet)
+# router.register(r'form', CVformViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+   # path('', include(router.urls)),
 ]
